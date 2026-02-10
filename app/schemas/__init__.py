f7aa35b0d38a11f032f952b23935b79e90069a1d
volodymyr <volodymyrtkach17@gmail.com>
2026-02-10 23:25:08 +0200

    Create projects and memberships pydantic schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .auth_schemas import RegisterRequest, JWTPayload, Token
+from .user_schemas import User, UserMetaName, UserUpdate
+from .membership_schemas import Membership
+from .projects_schemas import ProjectResponse, ProjectCreate, ProjectUpdate
