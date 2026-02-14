d15867d9ccfaaccfd261fbb5881b62d8488a7b90
Volodymyr Tkachenko <volodymyrtkach17@gmail.com>
2026-02-15 00:53:56 +0200

    fix

--- app/services/token_service.py
+++ app/services/token_service.py
@@ -3,7 +3,6 @@ from jose import ExpiredSignatureError, JWTError
 from ..schemas.auth_schemas import JWTPayload
 from ..core.security import create_jwt, decode_jwt
 
-
 class TokenService:
     @staticmethod
     def create_access_token(user_id: int | str) -> str:
