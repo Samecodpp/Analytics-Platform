7a74f817f004beb2945d421e4147d347b0086354
Volodymyr Tkachenko <volodymyrtkach17@gmail.com>
2026-02-14 18:11:50 +0200

    feature(repositories-users) Add get_by_email method

--- app/repositories/users_repo.py
+++ app/repositories/users_repo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import update
+from sqlalchemy import update, select
 
 from ..models import Users
 from .base_repo import BaseRepository
@@ -6,6 +6,10 @@ from .base_repo import BaseRepository
 class UserRepository(BaseRepository):
     def get_by_id(self, id: int) -> Users | None:
         return self.session.get(Users, id)
+
+    def get_by_email(self, email: str) -> Users | None:
+        stmt = select(Users).where(Users.email == email)
+        return self.session.scalar(stmt)
 
     def update_by_id(self, id: int, fields: dict) -> Users | None:
         stmt = update(Users).where(Users.id == id).values(**fields).returning(Users)
