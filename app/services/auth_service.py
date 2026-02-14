2bf8d15925b4a799fb25f718d5fc444cd71571dc
Volodymyr Tkachenko <volodymyrtkach17@gmail.com>
2026-02-14 20:20:52 +0200

    fix(service) Update AuthService acсording to UserRepository

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -13,7 +13,7 @@ class AuthService:
     def register(self, user_repo: UserRepository, creds: RegisterRequest) -> User:
         with user_repo:
             if user_repo.get_by_email(creds.email):
-                raise "User with this email already exist"  # TODO Own exception to this
+                raise "User with this email already exist" # TODO Own exception to this
 
             hashed_pwd = hash_password(creds.password)
             creds_dump = creds.model_dump(exclude={"password"})
@@ -24,34 +24,26 @@ class AuthService:
         return User.model_validate(new_user)
 
 
-    def login(self, db: Session, username: str, password: str, response: Response) -> Token:
-        user = db.query(Users).filter(
-            or_(username == Users.email, username == Users.username)
-        ).first()
+    def login(self, user_repo: UserRepository, email: str, password: str) -> dict:
+        with user_repo:
+            user = user_repo.get_by_email(email)
 
         if not user or not verify_password(password, user.hashed_password):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Username/password is invalid!",
-            )
+            raise "Username/password is invalid!" # TODO Own exception to this
 
         access_token = self.token_service.create_access_token(user.id)
         refresh_token = self.token_service.create_refresh_token(user.id)
 
-        self.token_service.set_refresh_cookie(response, refresh_token)
-        return Token(access_token=access_token)
+        return access_token, refresh_token
 
-    def refresh_access_token(self, refresh_token: str | None) -> Token:
+    def refresh_access_token(self, refresh_token: str | None) -> str:
         if not refresh_token:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+            raise "Refresh token does not exist" # TODO Own exception to this
 
         try:
             body = self.token_service.verify_refresh_token(refresh_token)
         except ValueError as e:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+            raise "Refresh token is invalid" # TODO Own exception to this
 
         new_access_token = self.token_service.create_access_token(user_id=body["sub"])
-        return Token(access_token=new_access_token)
-
-    def logout(self, response: Response) -> None:
-        self.token_service.delete_refresh_cookie(response)
+        return new_access_token
